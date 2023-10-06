c4d576b9b6ad5780eed83a662e3c62f8761f93d9
lukman nulhakim <lukman.judge45@gmail.com>
2023-10-06 11:14:47 +0700

    membuat register

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      jakarta: "Plus Jakarta Sans",
+      sacramento: "sacramento",
+      rubik: "rubik",
+    },
+    extend: {
+      colors: {
+        primary: "#FF8906",
+        secondary: "#F1F1F1",
+        brown: "#8E6447",
+        "dark-brown": "#542915",
+        "dark-gray": "#777C82",
+        gray: "#C3BEB7",
+        "light-gray": "#F8F8F8",
+        black: "#0B0909",
+      },
+    },
+  },
+  plugins: [],
+};

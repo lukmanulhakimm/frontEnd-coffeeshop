68ca15bae053445c00aecf60ecf795318f1f9b70
lukman nulhakim <lukman.judge45@gmail.com>
2023-10-08 22:28:42 +0700

    edit halaman product

--- public/js/global.js
+++ public/js/global.js
@@ -14,3 +14,22 @@ const menuDown = document.querySelector("#menu-down");
 dropDown.addEventListener("click", function () {
   menuDown.classList.toggle("hidden");
 });
+
+const modalTrigger = document.querySelector("#modal-trigger");
+const modalCancelBtn = document.querySelector("#modal-cancel-btn");
+const modalConfirmBtn = document.querySelector("#modal-confirm-btn");
+const modal = document.querySelector("#modal");
+
+modalTrigger.addEventListener("click", function () {
+  modal.classList.add("flex");
+  modal.classList.remove("hidden");
+});
+
+modalConfirmBtn.addEventListener("click", function () {
+  modal.classList.remove("flex");
+  modal.classList.add("hidden");
+});
+modalCancelBtn.addEventListener("click", function () {
+  modal.classList.remove("flex");
+  modal.classList.add("hidden");
+});
